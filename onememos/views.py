e1d6b80397aa54679a2923d727e89f05fa83c4e4
solyrion <sde101573@gmail.com>
2022-08-22 11:11:37 +0900

    .

--- onememos/views.py
+++ onememos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import *
+from . import models
 
 # Create your views here.
 """
@@ -17,10 +17,15 @@ render 함수와 template 파일의 관계
         - 그러나 템플릿 파일이 html 파일은 아니다. 장고만의 특수한 파일.
         - 이러한 파일은 대부분의 프레임워크에서도 마찬가자임. 전용파일개념
         - 또한 장고에서만 사용할 수 있는 문법(태크)등을 이러한 템플릿에 적용.->일반 html 파일 X
+        - 당연히 템플릿도 규칙과 최소한의 문법(태그)이 존재.
+        - 조건처리,반복처리 등이 가능 -> 얼핏 프로그래밍 언어처럼도 보이나 -> 언어라기 보다 전용표현정도
 """
 
 def index(request):
-    return HttpResponse("Onememos~ Hello, Wolrd!! 12341234")
+    # return HttpResponse("Onememos~ Hello, Wolrd!! 12341234")
+    # 이미 onememos까지 path설정이기에 '템플릿 경로'안에 쓸 필요 없음. 또한 templates폴더는 경로에 언급 x
+    return render(request, 'main.html')
+
 
 def createMemo(request):
     memoContent = request.GET['memoContent']
